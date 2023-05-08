ba738b45b5bf0f1fe45b80284e25fb67f5294b3c
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-05-08 21:57:17 +0300

    Add files via upload

---
+++ VizeCalisma1/VizeCalisma1/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tree t = new tree(new node(13));
+            t.addToTree(new node(3));
+            t.addToTree(new node(4));
+            t.addToTree(new node(12));
+            t.addToTree(new node(14));
+            t.addToTree(new node(10));
+            t.addToTree(new node(5));
+            t.addToTree(new node(1));
+            t.addToTree(new node(8));
+            t.addToTree(new node(2));
+            t.addToTree(new node(7));
+            t.addToTree(new node(9));
+            t.addToTree(new node(11));
+            t.addToTree(new node(6));
+            t.addToTree(new node(18));
+
+            t.preorder(t.root);
+            Console.WriteLine();
+            t.inorder(t.root);
+            Console.WriteLine();
+            t.postorder(t.root);
+
+        }
+    }
+
+    class node
+    {
+        public int data;
+        public node left;
+        public node right;
+        public node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    class tree
+    {
+        public node root;
+
+        public tree(node root)
+        {
+            this.root = root;
+        }
+
+        public void addToTree(node node)
+        {
+            if (this.root == null)
+            {
+                this.root = node;
+                return;
+            }
+            node tmp = this.root;
+            while (tmp != null)
+            {
+                if (node.data > tmp.data)
+                {
+                    if (tmp.right != null)
+                    {
+
+                        tmp = tmp.right;
+                    }
+                    else
+                    {
+                        tmp.right = node;
+                        return;
+                    }
+                }
+                else
+                {
+                    if (tmp.left != null)
+                    {
+                        tmp = tmp.left;
+                    }
+                    else
+                    {
+                        tmp.left = node;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public node searchNode(int x)
+        {
+            if (this.root == null)
+            {
+                Console.WriteLine("Agac bos.");
+            }
+            node temp = this.root;
+            while (temp != null)
+            {
+                if (temp.data == x)
+                {
+                    Console.WriteLine(x + " agacta bulundu.");
+                    return temp;
+                }
+                else
+                {
+                    if (temp.data > x)
+                    {
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        temp = temp.right;
+                    }
+                }
+            }
+            Console.WriteLine("Aranan deger (" + x + ") agacta yok.");
+            return null;
+        }
+
+        public void preorder(node x)
+        {
+
+            if (this.root == null)
+            {
+                return;
+            }
+            Console.Write(x.data + " ");
+            if (x.left != null)
+                preorder(x.left);
+            if (x.right != null)
+                preorder(x.right);
+        }
+
+        public void postorder(node x)
+        {
+
+            if (this.root == null)
+            {
+                return;
+            }
+            if (x.left != null)
+                preorder(x.left);
+            if (x.right != null)
+                preorder(x.right);
+            Console.Write(x.data + " ");
+        }
+
+        public void inorder(node x)
+        {
+
+            if (this.root == null)
+            {
+                return;
+            }
+            if (x.left != null)
+                preorder(x.left);
+            Console.Write(x.data + " ");
+            if (x.right != null)
+                preorder(x.right);
+        }
+
+        node minNode(node node)
+        {
+            if (node == null)
+            {
+                Console.Write("Agacin koku bos.\n");
+            }
+            node temp = node;
+            while (temp.left != null)
+            {
+                temp = temp.left;
+            }
+            return temp;
+        }
+
+        node maxNode(node node)
+        {
+            if (node == null)
+            {
+                Console.Write("Agacin rootu bos.\n");
+            }
+            node temp = node;
+            while (temp.right != null)
+            {
+                temp = temp.right;
+            }
+            return temp;
+        }
+
+        public bool isBST(node node)
+        {
+            if (node == null)
+                return true;
+
+            // nodun solundaki ağacın en büyük elemanının
+            // nodun değerinden büyükse ikili arama ağacı değildir.
+            if (node.left != null
+                && maxNode(node.left).data > node.data)
+                return false;
+
+            // nodun sağdaki ağacın en küçük elemanının
+            // nodun değerinden küçükse ikili arama ağacı değildir.
+            if (node.right != null
+                && minNode(node.right).data < node.data)
+                return false;
+
+            // Özyinelemeli olarak bakıyor.
+            if (isBST(node.left) == false || isBST(node.right) == false)
+                return false;
+
+            //bütün doğrulamalardan geçerse doğrudur.
+            return true;
+        }
+    }
+
+}
---
+++ VizeCalisma2/VizeCalisma2/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stack s = new stack();
+            s.smash(new node(8));
+            s.smash(new node(5));
+            s.smash(new node(4));
+            s.smash(new node(2));
+            s.printStack();
+            s.smash(new node(10));
+            s.printStack();
+        }
+    }
+
+    class node
+    {
+        public int veri;
+        public node ileri;
+        public node geri;
+
+        public node(int x)
+        {
+            veri = x;
+            ileri = null;
+            geri = null;
+        }
+    }
+
+    class stack
+    {
+        public node bas;
+        public node son;
+
+        public stack()
+        {
+            this.bas = null;
+            this.son = null;
+        }
+
+        public void push(node node)
+        {
+            if (bas == null)
+            {
+                this.bas = node;
+                this.son = node;
+            }
+            else
+            {
+                node.geri = this.son;
+                this.son.ileri = node;
+                this.son = node;
+            }
+        }
+
+        public int pop()
+        {
+            if (this.size() == 1)
+            {
+                int val = this.son.veri;
+                this.bas = null;
+                this.son = null;
+                return val;
+            }
+            else if (this.size() > 0)
+            {
+                int val = this.son.veri;
+                this.son = this.son.geri;
+                this.son.ileri = null;
+                return val;
+            }
+            else
+            {
+                this.bas = null;
+                this.son = null;
+                Console.WriteLine("Listede silmek için eleman yok.");
+                return -1;
+            }
+        }
+
+        public void printStack()
+        {
+            node temp = this.son;
+            while (temp != null)
+            {
+                Console.WriteLine(" | " + temp.veri + " | ");
+                temp = temp.geri;
+            }
+            Console.WriteLine();
+        }
+
+        public void smash(node n)
+        {
+            if (this.size() == 0)
+            {
+                this.push(n);
+            }
+            else
+            {
+                if (n.veri < this.peek())
+                {
+                    this.push(n);
+                }
+                else
+                {
+                    while (this.peek() < n.veri)
+                    {
+                        this.pop();
+                    }
+                    this.push(n);
+                }
+            }
+        }
+
+        public int size()
+        {
+            int count = 0;
+            node temp = this.bas;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.ileri;
+            }
+            return count;
+        }
+
+        public int peek()
+        {
+            if (this.size() > 0)
+                return this.son.veri;
+            return '0';
+        }
+
+        public void addtohead(node node)
+        {
+            if (bas == null)
+            {
+                this.bas = node;
+                this.son = node;
+            }
+            else
+            {
+                node.ileri = this.bas;
+                this.bas.geri = node;
+                this.bas = node;
+            }
+        }
+
+        public void addtoend(node node)
+        {
+            if (bas == null)
+            {
+                this.bas = node;
+                this.son = node;
+            }
+            else
+            {
+                node.geri = this.son;
+                this.son.ileri = node;
+                this.son = node;
+            }
+        }
+
+        public void addtomiddle(node node)
+        {
+            if (this.size() > 2)
+            {
+                node temp = this.bas;
+                for (int i = 0; i < this.size() / 2 - 1; i++)
+                {
+                    temp = temp.ileri;
+                }
+                node.geri = temp;
+                node.ileri = temp.ileri;
+                temp.ileri = node;
+            }
+            else
+            {
+                Console.WriteLine("Ortaya eklemek için listenin uzunluğu en az 3 olmalıdır.");
+            }
+        }
+
+        public void removefrommiddle()
+        {
+            if (this.size() > 2)
+            {
+                node temp = this.bas;
+                for (int i = 0; i < this.size() / 2 - 1; i++)
+                {
+                    temp = temp.ileri;
+                }
+                temp.ileri.ileri.geri = temp;
+                temp.ileri = temp.ileri.ileri;
+            }
+            else
+            {
+                Console.WriteLine("Ortadan silmek için listenin uzunluğu en az 3 olmalıdır.");
+            }
+        }
+
+        public void removefromhead()
+        {
+            if (this.size() > 0)
+            {
+                this.bas = this.bas.ileri;
+                this.bas.geri = null;
+            }
+            else
+            {
+                Console.WriteLine("Listede silmek için eleman yok.");
+            }
+        }
+
+        public void removefromend()
+        {
+            if (this.size() > 0)
+            {
+                this.son = this.son.geri;
+                this.son.ileri = null;
+            }
+            else
+            {
+                Console.WriteLine("Listede silmek için eleman yok.");
+            }
+        }
+
+    }
+
+
+}
